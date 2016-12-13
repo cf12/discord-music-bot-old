474240f5a007c18b4ced6ec13a0d46d8337dba70
Brian X <Brian X>
2016-12-12 23:08:39 -0500

    Migrating to Discord.JS

--- src/bot.js
+++ src/bot.js
@@ -1,5 +1,5 @@
 // Import Requirements
-const Eris = require('eris')
+const DiscordJS = require('discord.js')
 const fs = require('fs')
 const path = require('path')
 const ytdl = require('ytdl-core')
@@ -12,17 +12,18 @@ try {
 }
 
 // Object Construction
-const bot = new Eris(cfg.bot_token)
+const bot = new DiscordJS.Client()
 
 // Variable Decleration
 let pf = '$'
-let voiceConnection = undefined
+let voiceConnection
+let dispatcher
 let songQueue = []
 let volume = 1
 let mchannel
 
 // Init Bot
-bot.connect()
+bot.login(cfg.bot_token)
 
 /*
 // Functions
@@ -52,20 +53,19 @@ function parseYTUrl (url, callback) {
 
 // Function: Adds song to queue
 function addSong (videoID, member, callback) {
-  mchannel.createMessage('**INFO >** Fetching video information...')
-
   ytdl.getInfo('http://youtube.com/?v=' + videoID, {}, (err, info) => {
-    if (err) return mchannel.createMessage('**ERROR >** Error while parsing URL. Please make sure the URL is a valid YouTube link.')
+    if (err) return mchannel.sendMessage('**ERROR >** Error while parsing URL. Please make sure the URL is a valid YouTube link.')
+    mchannel.sendMessage('**INFO >** Fetching video information...')
     songQueue.push({
       video_ID: videoID,
       link: String('http://youtube.com/?v=' + videoID),
       title: info.title,
-      requester: member.mention,
+      requester: member.toString(),
       duration_seconds: info.length_seconds,
       duration: convertDuration(info.length_seconds)
     })
 
-    mchannel.createMessage('**INFO >** Song successfully added to queue.')
+    mchannel.sendMessage('**INFO >** Song successfully added to queue.')
 
     if (callback) callback()
     return true
@@ -75,26 +75,23 @@ function addSong (videoID, member, callback) {
 // Function: Plays next song
 function nextSong () {
   let song = songQueue[0]
-  voiceConnection.setVolume(volume)
 
-  mchannel.createMessage('**MUSIC >** __NOW PLAYING__: ' + song.title + ' - [' + song.duration + '] - requested by ' + song.requester)
-  voiceConnection.play(ytdl(song.link), { inlineVolume: true })
+  mchannel.sendMessage('**MUSIC >** __NOW PLAYING__: ' + song.title + ' - [' + song.duration + '] - requested by ' + song.requester)
+  return voiceConnection.playStream((ytdl(song.link), { volume: volume }))
 }
 
 // Function Connects to Voice Channel
-function voiceConnect (channelID) {
-  console.log('INFO > Joining Voice Channel <' + channelID + '>')
-  return bot.joinVoiceChannel(channelID)
+function voiceConnect (channel) {
+  console.log('INFO > Joining Voice Channel <' + channel.id + '>')
+  return channel.join()
 }
 
 // Function: Disconnect from Voice Channel
 function voiceDisconnect () {
   console.log('INFO > Leaving Voice Channel <' + voiceConnection.channelID + '>')
-  mchannel.createMessage('**INFO >** Queue ended. Disconnecting...')
+  mchannel.sendMessage('**INFO >** Queue ended. Disconnecting...')
 
-  voiceConnection.stopPlaying().then(() => {
-    voiceConnection.disconnect()
-  })
+  dispatcher.end()
 }
 
 /*
@@ -104,11 +101,11 @@ function voiceDisconnect () {
 // On: Bot ready
 bot.on('ready', () => {
   console.log('BOT > Music Bot started')
-  bot.editStatus('online', { name: 'v0.1.0 - By CF12' })
+  bot.user.setGame('v0.1.0 - By CF12')
 })
 
 // On: Message Creation
-bot.on('messageCreate', (msg) => {
+bot.on('message', (msg) => {
   // Cancel messages without pf or user is a bot
   if (msg.author.bot || !msg.content.startsWith(pf)) return
 
@@ -121,7 +118,7 @@ bot.on('messageCreate', (msg) => {
 
   // Command: Ping
   if (cmd === 'PING') {
-    msg.channel.createMessage('**INFO > **Pong!')
+    msg.channel.sendMessage('**INFO > **Pong!')
   }
 
   // Command: DB
@@ -132,7 +129,7 @@ bot.on('messageCreate', (msg) => {
   // Command: Play from YouTube Link
   if (cmd === 'PLAY') {
     let videoID
-    if (!member.voiceState.channelID) return mchannel.createMessage('**ERROR >** User is not in a voice channel!')
+    if (!member.voiceChannel) return mchannel.sendMessage('**ERROR >** User is not in a voice channel!')
 
     if (args.length === 0) return mchannel.createChannel('**INFO >** Adds a YouTube link to the playlist. Usage: *' + pf + 'play [url]*')
     else if (args.length > 1) return mchannel.createChannel('**ERROR >** Invalid usage! Usage: *' + pf + 'play [url]*')
@@ -141,25 +138,23 @@ bot.on('messageCreate', (msg) => {
       videoID = parseYTUrl(args[0])
     } catch (err) {
       console.log('ERROR > ' + err)
-      return mchannel.createMessage('**ERROR >** Error while parsing URL. Please make sure the URL is a valid YouTube link.')
+      return mchannel.sendMessage('**ERROR >** Error while parsing URL. Please make sure the URL is a valid YouTube link.')
     }
 
     addSong(videoID, msg.member, () => {
-      if (!voiceConnection || !voiceConnection.playing) {
-        voiceConnect(member.voiceState.channelID)
+      if (!voiceConnection) {
+        voiceConnect(member.voiceChannel)
         .then((connection) => {
           voiceConnection = connection
           nextSong()
-
-          // connection
-          // .on('debug', (msg) => {
-          //   console.log(msg)
-          // })
-
-          .on('end', () => {
-            songQueue.shift()
-            if (songQueue.length === 0) return voiceDisconnect()
-            nextSong()
+          .then((streamDispatcher) => {
+            dispatcher = streamDispatcher
+            dispatcher
+            .once('end', () => {
+              songQueue.shift()
+              if (songQueue.length === 0) return voiceDisconnect()
+              nextSong()
+            })
           })
         })
       }
@@ -168,17 +163,17 @@ bot.on('messageCreate', (msg) => {
 
   // Command: Leave Voice Channel
   if (cmd === 'LEAVE') {
-    if (!voiceConnection.playing || !voiceConnection) return mchannel.createMessage('**ERROR >** The bot is not in a voice channel!')
+    if (!voiceConnection) return mchannel.sendMessage('**ERROR >** The bot is not in a voice channel!')
     voiceDisconnect()
   }
 
   // Command: Volume Control
   if (cmd === 'VOLUME') {
     if (args.length === 0) {
-      mchannel.createMessage('**INFO >** Sets the volume of music. Usage: *' + pf + 'volume [1-100]*')
+      mchannel.sendMessage('**INFO >** Sets the volume of music. Usage: *' + pf + 'volume [1-100]*')
     } else if (args.length === 1 && args[0] <= 100 && args[0] >= 1) {
-      voiceConnection.setVolume(args[0] + 50)
-      mchannel.createMessage('**INFO >** Volume set to: ' + args[0])
-    } else mchannel.createMessage('**ERROR >** Invalid usage! Usage: *' + pf + 'volume [1-100]*')
+      if (dispatcher) dispatcher.setVolume(args[0] * 0.01)
+      mchannel.sendMessage('**INFO >** Volume set to: ' + args[0])
+    } else mchannel.sendMessage('**ERROR >** Invalid usage! Usage: *' + pf + 'volume [1-100]*')
   }
 })
