c010592b79afea973a78cd7b779a1dd3257faceb
Brian X <Brian X>
2016-12-17 23:05:31 -0500

    - Added EXTREMELY fast request module - Implemented volume, playlist, and other stuff (mostly bug fixes) - Working on radios

--- src/bot.js
+++ src/bot.js
@@ -3,6 +3,7 @@ const DiscordJS = require('discord.js')
 const fs = require('fs')
 const path = require('path')
 const ytdl = require('ytdl-core')
+const YouTubeAPIHandler = require('./youtubeapihandler')
 
 // Load Files
 try {
@@ -13,86 +14,19 @@ try {
 
 // Object Construction
 const bot = new DiscordJS.Client()
+const yth = new YouTubeAPIHandler(cfg.youtube_api_key)
 
 // Variable Decleration
 let pf = '$'
 let voiceConnection
+let voiceChannel
 let dispatcher
 let songQueue = []
-let volume = 1
+let volume = 0.15
 let mchannel
 
 // Init Bot
 bot.login(cfg.bot_token)
-
-/*
-// Functions
-*/
-
-// Function: Convert Duration of times from seconds to standard [hh:mm:ss] format
-function convertDuration (time) {
-  let hours = parseInt(time / 3600, 10) % 24
-  let minutes = parseInt(time / 60, 10) % 60
-  let seconds = time % 60
-  if (hours >= 1) return (hours < 10 ? '0' + hours : hours) + ':' + (minutes < 10 ? '0' + minutes : minutes) + ':' + (seconds < 10 ? '0' + seconds : seconds)
-  if (minutes >= 1) return (minutes < 10 ? '0' + minutes : minutes) + ':' + (seconds < 10 ? '0' + seconds : seconds)
-  else return '00:' + (seconds < 10 ? '0' + seconds : seconds)
-}
-
-// Function: Parse YT Url
-function parseYTUrl (url, callback) {
-  let videoID
-
-  if (url.includes('youtube.com') && url.includes('watch?v=')) videoID = url.split('watch?v=')[1].split('#')[0].split('&')[0]
-  else if (url.includes('youtu.be')) videoID = url.split('be/')[1].split('?')[0]
-  else return undefined
-
-  if (callback) return callback(videoID)
-  return videoID
-}
-
-// Function: Adds song to queue
-function addSong (videoID, member, callback) {
-  ytdl.getInfo('http://youtube.com/?v=' + videoID, {}, (err, info) => {
-    if (err) return mchannel.sendMessage('**ERROR >** Error while parsing URL. Please make sure the URL is a valid YouTube link.')
-    mchannel.sendMessage('**INFO >** Fetching video information...')
-    songQueue.push({
-      video_ID: videoID,
-      link: String('http://youtube.com/?v=' + videoID),
-      title: info.title,
-      requester: member.toString(),
-      duration_seconds: info.length_seconds,
-      duration: convertDuration(info.length_seconds)
-    })
-
-    mchannel.sendMessage('**INFO >** Song successfully added to queue.')
-
-    if (callback) callback()
-    return true
-  })
-}
-
-// Function: Plays next song
-function nextSong () {
-  let song = songQueue[0]
-
-  mchannel.sendMessage('**MUSIC >** __NOW PLAYING__: ' + song.title + ' - [' + song.duration + '] - requested by ' + song.requester)
-  return voiceConnection.playStream((ytdl(song.link), { volume: volume }))
-}
-
-// Function Connects to Voice Channel
-function voiceConnect (channel) {
-  console.log('INFO > Joining Voice Channel <' + channel.id + '>')
-  return channel.join()
-}
-
-// Function: Disconnect from Voice Channel
-function voiceDisconnect () {
-  console.log('INFO > Leaving Voice Channel <' + voiceConnection.channelID + '>')
-  mchannel.sendMessage('**INFO >** Queue ended. Disconnecting...')
-
-  dispatcher.end()
-}
 
 /*
 // Bot Events
@@ -106,7 +40,7 @@ bot.on('ready', () => {
 
 // On: Message Creation
 bot.on('message', (msg) => {
-  // Cancel messages without pf or user is a bot
+  // Cancels messages without pf or user is a bot
   if (msg.author.bot || !msg.content.startsWith(pf)) return
 
   // Command variables
@@ -128,33 +62,49 @@ bot.on('message', (msg) => {
 
   // Command: Play from YouTube Link
   if (cmd === 'PLAY') {
-    let videoID
+    let sourceID
     if (!member.voiceChannel) return mchannel.sendMessage('**ERROR >** User is not in a voice channel!')
 
-    if (args.length === 0) return mchannel.createChannel('**INFO >** Adds a YouTube link to the playlist. Usage: *' + pf + 'play [url]*')
-    else if (args.length > 1) return mchannel.createChannel('**ERROR >** Invalid usage! Usage: *' + pf + 'play [url]*')
+    if (args.length === 0) return mchannel.sendMessage('**INFO >** Adds a YouTube link to the playlist. Usage: *' + pf + 'play [url]*')
+    else if (args.length > 1) return mchannel.sendMessage('**ERROR >** Invalid usage! Usage: ' + pf + 'play [url]')
 
     try {
-      videoID = parseYTUrl(args[0])
+      sourceID = parseYTUrl(args[0])
     } catch (err) {
       console.log('ERROR > ' + err)
       return mchannel.sendMessage('**ERROR >** Error while parsing URL. Please make sure the URL is a valid YouTube link.')
     }
 
-    addSong(videoID, msg.member, () => {
-      if (!voiceConnection) {
-        voiceConnect(member.voiceChannel)
-        .then((connection) => {
-          voiceConnection = connection
-          nextSong()
-          .then((streamDispatcher) => {
-            dispatcher = streamDispatcher
-            dispatcher
-            .once('end', () => {
+    if (sourceID.includes('p:')) {
+      let playlistID = sourceID.substring(2)
+      console.log(playlistID)
+      addPlaylist(playlistID, member, () => {
+        if (!voiceConnection) {
+          voiceConnect(member.voiceChannel)
+          .then((connection) => {
+            voiceConnection = connection
+            dispatcher = nextSong()
+            .on('end', () => {
               songQueue.shift()
               if (songQueue.length === 0) return voiceDisconnect()
               nextSong()
             })
+          })
+        }
+      })
+      return
+    }
+
+    addSong(sourceID, msg.member, false, () => {
+      if (!voiceConnection) {
+        voiceConnect(member.voiceChannel)
+        .then((connection) => {
+          voiceConnection = connection
+          dispatcher = nextSong()
+          .on('end', () => {
+            songQueue.shift()
+            if (songQueue.length === 0) return voiceDisconnect()
+            nextSong()
           })
         })
       }
@@ -164,16 +114,93 @@ bot.on('message', (msg) => {
   // Command: Leave Voice Channel
   if (cmd === 'LEAVE') {
     if (!voiceConnection) return mchannel.sendMessage('**ERROR >** The bot is not in a voice channel!')
-    voiceDisconnect()
+    songQueue = []
+    dispatcher.end()
+    voiceConnection = undefined
   }
 
   // Command: Volume Control
   if (cmd === 'VOLUME') {
-    if (args.length === 0) {
-      mchannel.sendMessage('**INFO >** Sets the volume of music. Usage: *' + pf + 'volume [1-100]*')
-    } else if (args.length === 1 && args[0] <= 100 && args[0] >= 1) {
-      if (dispatcher) dispatcher.setVolume(args[0] * 0.01)
+    if (args.length === 0) return mchannel.sendMessage('**INFO >** Sets the volume of music. Usage: ' + pf + 'volume [1-100]')
+    if (args.length === 1 && args[0] <= 100 && args[0] >= 1) {
+      volume = args[0] * 0.002
+      if (dispatcher) dispatcher.setVolume(volume)
       mchannel.sendMessage('**INFO >** Volume set to: ' + args[0])
-    } else mchannel.sendMessage('**ERROR >** Invalid usage! Usage: *' + pf + 'volume [1-100]*')
+    } else mchannel.sendMessage('**ERROR >** Invalid usage! Usage: ' + pf + 'volume [1-100]')
   }
 })
+
+/*
+// Functions
+*/
+
+// Function: Parse YT Url
+function parseYTUrl (url, callback) {
+  let videoID
+
+  if (url.includes('youtube.com') && url.includes('watch?v=')) videoID = url.split('watch?v=')[1].split('#')[0].split('&')[0]
+  else if (url.includes('youtu.be')) videoID = url.split('be/')[1].split('?')[0]
+  else if (url.includes('youtube.com') && url.includes('playlist?list=')) videoID = 'p:' + url.split('playlist?list=')[1]
+  else return undefined
+
+  if (callback) return callback(videoID)
+  return videoID
+}
+
+// Function: Adds song to queue
+function addSong (videoID, member, suppress, callback) {
+  if (!suppress) mchannel.sendMessage('**INFO >** Fetching video information...')
+  yth.getVideo(videoID, (err, info) => {
+    if (err) return mchannel.sendMessage('**ERROR >** Error while parsing video(es). Please make sure the URL is valid.')
+    let video = info.items[0]
+    songQueue.push({
+      video_ID: videoID,
+      link: String('http://youtube.com/?v=' + videoID),
+      requester: member.toString(),
+      title: video.snippet.title,
+      duration: video.contentDetails.duration
+    })
+
+    if (!suppress) mchannel.sendMessage('**INFO >** Song successfully added to queue.')
+    if (callback) callback()
+  })
+}
+
+// Function: Queues an entire playlist
+function addPlaylist (playlistID, member, callback) {
+  mchannel.sendMessage('**INFO >** Fetching playlist information...')
+  yth.getPlaylist(playlistID, (err, playlist) => {
+    if (err) return mchannel.sendMessage('**ERROR >** Error while parsing playlist URL. Please make sure the URL is valid.')
+
+    for (const index in playlist) {
+      addSong(playlist[index].snippet.resourceId.videoId, member, true, () => {
+        if ((playlist[0] === playlist[index]) && (typeof callback === 'function')) callback()
+      })
+    }
+  })
+}
+
+// Function: Plays next song
+function nextSong () {
+  let song = songQueue[0]
+
+  mchannel.sendMessage('**MUSIC >** __NOW PLAYING__: ' + song.title + ' - [' + song.duration + '] - requested by ' + song.requester)
+  return voiceConnection.playStream((ytdl(song.link)), {
+    'volume': volume
+  })
+}
+
+// Function Connects to Voice Channel
+function voiceConnect (channel) {
+  voiceChannel = channel
+  console.log('INFO > Joining Voice Channel <' + voiceChannel.id + '>')
+  return channel.join()
+}
+
+// Function: Disconnect from Voice Channel
+function voiceDisconnect () {
+  console.log('INFO > Leaving Voice Channel <' + voiceChannel.id + '>')
+  mchannel.sendMessage('**INFO >** Queue ended. Disconnecting...')
+
+  voiceConnection.disconnect()
+}
---
+++ src/youtubeapihandler.js
@@ -0,0 +1,67 @@
+const req = require('req-fast')
+
+module.exports = class YTApiHandler {
+  constructor (apiKey) {
+    this.apiKey = apiKey
+    this.options = {
+      base: 'https://www.googleapis.com/youtube/v3'
+    }
+
+    this.resultPlaylist = []
+  }
+
+  getVideo (videoID, callback) {
+    let options = {
+      url: this.options.base + '/videos?part=snippet,contentDetails&id=' + videoID + '&key=' + this.apiKey,
+      method: 'GET',
+      dataType: 'json'
+    }
+
+    req(options, (err, res) => {
+      if (!err && res.statusCode === 200) {
+        return callback(undefined, (res.body))
+      } else return callback(err, undefined)
+    })
+  }
+
+  getPlaylist (playlistID, callback) {
+    let options = {
+      url: this.options.base + '/playlistItems/?part=snippet,contentDetails&maxResults=50&playlistId=' + playlistID + '&key=' + this.apiKey,
+      method: 'GET',
+      dataType: 'json'
+    }
+
+    req(options, (err, res) => {
+      if (!err && res.statusCode === 200) {
+        if (res.body.nextPageToken) {
+          try {
+            this.getCompletePlaylist(playlistID, res.body.nextPageToken, (playlist) => {
+              callback(undefined, playlist)
+            })
+          } catch (err) {
+            callback(err, undefined)
+          }
+        } else {
+          callback(undefined, res.body.items)
+        }
+      } else callback(err, undefined)
+    })
+  }
+
+  getCompletePlaylist (playlistID, pageToken, callback) {
+    req({
+      url: this.options.base + '/playlistItems/?part=snippet,contentDetails&maxResults=50&playlistId=' + playlistID + '&key=' + this.apiKey + '&pageToken=' + pageToken,
+      method: 'GET',
+      dataType: 'json'
+    }, (err, res) => {
+      if (!err && res.statusCode === 200) {
+        this.resultPlaylist = this.resultPlaylist.concat(res.body.items)
+        if (res.body.nextPageToken) pageToken = res.body.nextPageToken
+        else pageToken = undefined
+
+        if (pageToken) this.getCompletePlaylist(playlistID, pageToken, callback)
+        if (!pageToken && callback) callback(this.resultPlaylist)
+      } else throw (err)
+    })
+  }
+}
